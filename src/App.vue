34e0bf67a893598c37258b774f8c3f9ea316339f
Salvatore Pitanza <pitanza.salvatore.86@gmail.com>
2022-06-14 15:45:23 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,101 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <main class="py-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 py-5 text-center">
+            <input type="text" placeholder="cerca personaggio" v-model="text" />
+          </div>
+          <div v-if="!filteredCharacters.length" class="text-center">
+            <h2>la ricerca non ha prodotto nessun risultato</h2>
+          </div>
+          <div
+            v-else
+            class="col text-center py-4"
+            v-for="(character, index) in filteredCharacters"
+            :key="index"
+          >
+            <img :src="character.image" class="shadow" alt="" />
+            <h2>{{ character.name }}</h2>
+            <div class="d-flex justify-content-center py-2">
+              <div class="linea"></div>
+            </div>
+            <p>Specie: {{ character.species }}</p>
+            <p >Location: {{ character.location.name }}</p>
+          </div>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue";
+import axios from "axios";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Header,
+  },
+  data() {
+    return {
+      characters: [],
+      text: "",
+    };
+  },
+  computed: {
+    filteredCharacters() {
+      const search = this.text.toLowerCase();
+      return this.characters.filter((character) => {
+        return character.name.toLowerCase().includes(search);
+      });
+    },
+  },
+  methods: {
+    getCharacters() {
+      axios
+        .get("https://rickandmortyapi.com/api/character/")
+        .then((res) => {
+          this.characters = res.data.results;
+          console.log(this.characters);
+        })
+        .catch((err) => {
+          console.error(err);
+        })
+        .then(() => {
+          console.log("chiamata terminata");
+        });
+    },
+  },
+  mounted() {
+    this.getCharacters();
+  },
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import "./assets/scss/app.scss";
+input{
+      border: 0;
+    border-radius: 10px;
+    padding: 10px;
+    width: 300px;
+    outline: 0;
+    transition: 0.5s ease;
+}
+input:focus-visible{
+      box-shadow: black 0px 0px 10px -4px;
+
+}
+
+img {
+  border-radius: 50%;
+}
+
+.linea{
+  border-bottom: 2px solid black;
+  width: 150px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <nav class="navbar navbar-expand-lg bg-light position-fixed w-100">
+      <div class="container-fluid">
+        <h1 class="navbar-brand">Rick & Morty Search</h1>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped lang="scss">
+</style>
